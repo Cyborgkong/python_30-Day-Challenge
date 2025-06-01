a40f2da97dd92c2ebf8fd8cead2fd8cc8568c78b
Cyborgkong <adammoses322@gmail.com>
2025-06-01 02:17:50 -0700

    day10

---
+++ day10_Banking_System.py
@@ -0,0 +1,68 @@
+import json
+import os
+
+FILE_PATH = 'json Projects/bank_data.json'
+
+def load_data():
+    if not os.path.exists(FILE_PATH):
+        return {}  # Return dict instead of list
+    with open(FILE_PATH, 'r') as file:
+        return json.load(file)
+    
+def save_data(data):
+    with open(FILE_PATH, 'w') as file:
+        json.dump(data, file, indent=4)
+        
+def check_balance(name):
+    if name in users:
+        print(f"{name}'s balance is: {users[name]['balance']}")
+    else:
+        print("User not found.")
+        
+def deposit(name, amount):
+    if name in users:
+        users[name]['balance'] += amount
+        print(f"{amount} deposited. New balance: {users[name]['balance']}.")
+    else:
+        print("User not found.")
+        
+def withdraw(name, amount):
+    if name in users:
+        if users[name]['balance'] >= amount:
+            users[name]['balance'] -= amount
+            print(f"{amount} withdrawn. New balance: {users[name]['balance']}.")
+        else:
+            print("Insufficient funds.")
+    else:
+        print("User not found.")
+     
+users = load_data()   
+save_data(users)
+
+while True:
+    print("\nMenu")
+    print("1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+    
+    choice = input("Enter choice: ")
+    
+    if choice == '1':
+        name = input("Enter user name: ")
+        check_balance(name)
+    elif choice == '2':
+        name = input("Enter user name: ")
+        amount = float(input("Enter amount: "))
+        deposit(name, amount)
+        save_data(users)
+    elif choice == '3':
+        name = input("Enter user name: ")
+        amount = float(input("Enter amount: "))
+        withdraw(name, amount)
+        save_data(users)
+    elif choice == '4':
+        print("Exiting....")
+        break
+    else:
+        print("Invalid choice. Try again")
--- day9_json_db.py
+++ day9_json_db.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-FILE_PATH = 'users.json'
+FILE_PATH = 'json Projects\users.json'
 
 def load_data():
     if not os.path.exists(FILE_PATH):
