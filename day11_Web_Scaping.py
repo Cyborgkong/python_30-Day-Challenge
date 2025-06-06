16f1d495af1bc12a6bff9021708980d9ecc1e31b
Cyborgkong <adammoses322@gmail.com>
2025-06-06 16:07:24 -0700

    day11_update

--- day11_Web_Scaping.py
+++ day11_Web_Scaping.py
@@ -1,18 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-url = 'http://quotes.toscrape.com'
+base_url = 'http://quotes.toscrape.com'
+next_page = '/page/1/'
 
-response = requests.get(url)
-if response.status_code == 200:
-    print("Page fetched successfully!")
-else:
-    print(f"Failed to fetch page. Status code {response.status_code}")
-    
-soup = BeautifulSoup(response.text, 'html.parse')
+all_quotes = []
 
-quotes = soup.find_all('div', class_='quote')
-for quote in quotes:
-    text = quote.find('span', class_='text').get_text()
-    author = quote.find('small', class_='author').get_text()
-    print(f'"{text}" - {author}')
+while next_page:
+    response = requests.get(base_url + next_page)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    quotes = soup.find_all('div', class_='quote')
+    for quote in quotes:
+        text = quote.find('span', class_='text').get_text(strip=True)
+        author = quote.find('small', class_='author').get_text(strip=True)
+        tags = [tag.get_text(strip=True) for tag in quote.find_all('a', class_='tag')]
+        
+        all_quotes.append({
+            'text': text,
+            'author': author,
+            'tags': tags
+        })
+
+    next_btn = soup.find('li', class_='next')
+    next_page = next_btn.find('a')['href'] if next_btn else None
+
+# Save to JSON file
+with open('all_quotes.json', 'w') as file:
+    json.dump(all_quotes, file, indent=4)
+
+print(f"Scraped {len(all_quotes)} quotes. Saved to 'all_quotes.json'.")
